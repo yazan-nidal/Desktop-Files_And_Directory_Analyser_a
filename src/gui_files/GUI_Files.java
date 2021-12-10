519ae38b345065598782c505d78b915994c99603
yazan-nidal <yazan.nidal@hotmail.com>
2021-12-11 00:34:09 +0200

    initial commit

---
+++ src/gui_files/GUI_Files.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui_files;
+
+/**
+ *
+ * @author ads
+ */
+public class GUI_Files {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Display_All_Files().setVisible(true);
+    }
+    
+}
